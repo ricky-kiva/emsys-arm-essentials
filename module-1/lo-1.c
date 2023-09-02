ea6cae940ef013b5be1bf1611936bef4e140d5e8
ricky-kiva <ricky.kiva@gmail.com>
2023-09-02 14:48:07 +0700

    feat(lo): add learning outcome 1 summary

---
+++ module-1/lo-1.c
@@ -0,0 +1,70 @@
+// 1. Explain an embedded system, its components, and its applications
+
+// embedded system: specialized computer system designed to perform specific task within a larger system
+// 3 main components:
+// - microcontroller/microprocessor: the brain
+// - memory: stores program instructions & data
+// - I/O interfaces: connect to external world
+// applications: everyday devices (washing machines, ovens, cars, industrial robots, IoT gadgets, etc)
+// - note: it's essential for automation, control, & monitoring
+
+// 2. Compare and contrast a CPU, an MCU, and an embedded system
+
+// CPU (central processing unit): core component to execute instructions & calculations
+// - examples: Intel Core i7, AMD Ryzen 9, Apple M1, etc
+// MCU (microcontroller unit): compact computing device that combines CPU, memory, & I/O peripherals on a single chip
+// - typically used in embedded systems for specific tasks (sensors, controlling appliances)
+// - examples: Arduino, Raspberry Pi, STM32 MCU, etc
+// embedded system: broad concept encompassing CPUs, MCUs, & software, all working together to perform dedicated functions
+// - examples: smartphones, automotive engine control units (ECUs), smart home thermostats, etc
+
+// 3. Explain the role of embedded systems in the Internet of Things (IoT) and the context of ubiquitous computing
+
+// Ubiquitous computing (ubicomp) is a concept where computing technology is seamlessly integrated into our everyday surroundings
+// - in this vision, computing becomes an invisible part of the environment, allowing people to interact with digital services effortlessly
+// in IoT, embedded systems enable everyday objects to collect, process, and transmit data to internet, facilitating remote monitoring & control
+// - this network of devices creates seamless web of information exchange, giving rise to ubiquitous computing, where computing capabilities serving as heart of the interconnected devices
+
+// 4. Identify the challenges of incorporating embedded devices into IoT systems
+
+// security: devices are vulnerable to cyberattacks
+// scalability: concern when managing vast number of devices
+// interoperability: diversity in hardware and communication protocols
+// power efficiency: managing battery-powered device lifespans are vital
+// data-related: privacy, regulations, and analysis of sheer volume of data
+// regular software updates & maintenance
+
+// 5. Explain the benefits of microcontroller-based embedded systems
+
+// compact, cost-effective, power-efficient, ideal for applications with constrained resources
+// real-time processing capabilities enable precise control & timing
+// simplicity & ease of programming facilitate rapid development
+// can be tailored to specific tasks, reducing unnecessary oerhead
+// low power consumption, extends battery life in portable devices
+
+// 6. Identify important trade-offs of embedded systems, such as cost and performance
+
+// optimizing power efficiency might sacrifices processing speed/features
+// achieving high performance requires more powerful hardware, which can increase manufacturing & development costs
+
+// 7. Identify the characteristics of Arm architecture and processors as core embedded systems components for the IoT.
+
+// known for low energy consumption, ideal for batteryAr-powered IoT devices
+// the scalability allows IoT solutions to range from small, resource-constrained devices, to more powerful edge computing nodes
+// Arm's widespread adoption & extensive ecosystem offer a wide variety of development tools, libraries, and support. enable rapid IoT product development
+// it often integrate security features, crucial for IoT that transmit sensitive data
+
+// X1. Technical Terminologies
+
+// ALU: Arithmetic Logic Unit (crucial part of CPU that performs calculations & logic operations)
+// SoC: System on a Chip (mini-computer on a single chip that combines CPU, memory, I/O interfaces, sometimes graphic or networking components, etc)
+// - commonly used in smartphones, IoT, to save space & power while providing necessary computing capabilities in 1 package
+// - example: Qualcomm's Snapdragon SoCs (integrate CPU, GPU, modem, image signal processor (ISP), etc, onto a single chip)
+// MCU: Microcontroller Unit
+// CPU: Central Processing Unit
+// IoT: Internet of Things
+
+// X2. Look out!
+
+// The complexity of the IoT, once you start digging deeper. Get the big picture, but then recognize that your expertise may develop in just one particular area.
+// The complexity of the Arm product range. Again, get the big picture, but recognize that we focus on just one microcontroller type, and its support environment.
